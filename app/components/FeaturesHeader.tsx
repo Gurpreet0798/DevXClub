280c2ad3492b142d18450cc7d26fc40236a66073
Gurpreet Kaur <118358763+Gurpreet0798@users.noreply.github.com>
2024-05-01 16:35:01 +0530

    Update FeaturesHeader.tsx

--- app/components/FeaturesHeader.tsx
+++ app/components/FeaturesHeader.tsx
@@ -6,7 +6,7 @@ const FeaturesHeader = () => {
     return (
         <div className='flex my-16 flex-col items-center justify-center'>
             <Button text='many features,wow' />
-            <p className='text-4xl mt-[-20px] font-semibold'> What's in DevXClub?</p>
+            <p className='text-4xl mt-[-20px] font-semibold'> What is DevXClub?</p>
             <p className='text-gray-400 mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
     )
